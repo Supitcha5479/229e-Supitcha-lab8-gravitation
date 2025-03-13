2e2ca59a3355dddac17d2e0c90815967db4be70a
Supitcha5479 <supitcha.prat@bumail.net>
2025-03-13 10:05:28 +0700

    Attract สคริป
    แรงดึงดูด2วัตถุ

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Rigidbody rb;
+    const float G = 0.00674f; //ค่าที่ห้ามเปลี่ยน
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+    }
+    void Attract(Gravity other)
+    { 
+    Rigidbody otherRb = other.rb;
+    Vector3 direction = rb.position - otherRb.position; //หาระยะทางของสองสิ่ง ได้ระยะกับทิศ
+        float distance = direction.magnitude; //เอาแค่ระยะทางมาใช้
+        float forceMagnitude = G * (rb.mass * otherRb.mass)/ Mathf.Pow(distance,2); //Matf สูตรคณิตในC# อันนี้มีเฉพาะแรง
+        Vector3 finalForce = forceMagnitude * direction.normalized; //nor คือทิศ
+        otherRb.AddForce(finalForce);
+
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }

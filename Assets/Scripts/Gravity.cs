5d1d598c747c8cd777adab404f7f2c4ead526ae3
Supitcha5479 <supitcha.prat@bumail.net>
2025-03-13 10:45:55 +0700

    สคริปเรียบร้อย

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -1,13 +1,41 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic; //ไว้ให้ใช้ List ได้
 
 public class Gravity : MonoBehaviour
 {
     Rigidbody rb;
     const float G = 0.00674f; //ค่าที่ห้ามเปลี่ยน
+    public static List<Gravity> gravityObjectList;
+
+    //orbit โคจร
+    [SerializeField] bool planets = false; //ตัวบอกว่าไม่ใช่ตัวจุดศูนย์กลาง
+    [SerializeField] int orbitSpeed = 1000;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        if (gravityObjectList == null)
+        {
+            gravityObjectList = new List<Gravity>();
+        }
+
+        gravityObjectList.Add(this);
+
+        //เริ่มโคจรก่อนถูกดูด
+        if (!planets)
+        {
+            rb.AddForce(Vector3.left * orbitSpeed);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (var obj in gravityObjectList) //ของในList แต่ละอันจะทำอะไรบางอย่างใน {}
+        {
+            if (obj != this) { Attract(obj); } //ดูดคนอื่น
+            
+        }
     }
     void Attract(Gravity other)
     { 
